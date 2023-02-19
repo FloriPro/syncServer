0938eba04eab4cec9d465b8f26bd2966fc4e1e12
Florian Lohner <floriprolohner@gmail.com>
2023-02-19 13:33:34 +0100

    v1

--- db.py
+++ db.py
@@ -1,8 +1,80 @@
 import os
 import env
 from supabase import create_client, Client
+import hashlib
+import traceback
 
 API_URL = os.environ['SUPA_API_URL']
 API_KEY = os.environ['SUPA_API_KEY']
 
 supabase: Client = create_client(API_URL, API_KEY)
+
+def hash(dat):
+    return hashlib.md5(dat.encode('utf-8')).hexdigest()
+
+def getUser(name: str):
+    user = supabase.table("users").select("*").eq("name", name).execute()
+    if len(user.data) == 0:
+        return {"name": None, "password": None}
+    return user.data[0]
+
+
+def addUser(name: str, password: str):
+    try:
+        supabase.table("users").insert(
+            {"name": name, "password": password}).execute()
+        return True
+    except:
+        return False
+
+
+def userExists(name: str):
+    return getUser(name)["name"] != None
+
+
+def getChangeTime(user: str, path: str):
+    q = supabase.table("files").select("changeTime").eq(
+        "user", user).eq("path", path).execute().data
+    if len(q) == 0:
+        return None
+    return q[0]["changeTime"]
+
+
+def fileExists(user: str, path: str):
+    return getChangeTime(user, path) != None
+
+
+def setFile(user: str, path: str, data: str, changeTime: int):
+    try:
+        supabase.table("files").insert(
+            {"user": user,
+             "path": path,
+             "content": data,
+             "changeTime": changeTime,
+             "md5": hash(data)}).execute()
+        return True
+    except:
+        return False
+
+
+def removeFile(user: str, path: str):
+    try:
+        r = supabase.table("files").delete().eq(
+            "user", user).eq("path", path).execute()
+        if len(r.data) == 0:
+            return False
+        return True
+    except:
+        return False
+
+def getHashes(user: str):
+    q = supabase.table("files").select("md5", "path", "changeTime").eq("user", user).execute().data
+    return q
+
+def getFile(user: str, path: str):
+    q = supabase.table("files").select("content").eq("user", user).eq("path", path).execute().data
+    if len(q) == 0:
+        return None
+    return q[0]["content"]
+
+print("connected")
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import traceback
 import db
 from simple_websocket_server import WebSocketServer, WebSocket
 import json
@@ -9,46 +10,135 @@ def genHash(dat):
     return hashlib.md5(dat.encode('utf-8')).hexdigest()
 
 
+def sha(dat):
+    return hashlib.sha512(dat.encode('utf-8')).hexdigest()
+
+
 users = {}
 
 
 class Serv(WebSocket):
 
     def handle(self):
-        global users
-        data = json.loads(self.data)
-        if data["type"] == "auth":
-            pass
+        try:
+            global users
+            data = json.loads(self.data)
+            if data["type"] == "auth":
+                user = db.getUser(data["username"])
+                if user["name"] == None:
+                    self.send({
+                        "type": "auth",
+                        "status": False,
+                        "message": "Username not found"
+                    })
+                    return
+                if user["password"] != sha(data["password"]):
+                    self.send({
+                        "type": "auth",
+                        "status": False,
+                        "message": "Incorrect password"
+                    })
+                    return
 
-        elif data["type"] == "register":
-            pass
+                # auth success
+                self.authenticated = True
+                self.authenticatedAs = data["username"]
+                if self.authenticatedAs not in users:
+                    users[self.authenticatedAs] = []
+                users[self.authenticatedAs].append(self)
 
-        elif data["type"] == "change":
-            pass
+                self.send({"type": "auth", "status": True, "dat": "login"})
 
-        elif data["type"] == "delete":
-            pass
+            elif data["type"] == "register":
+                if db.userExists(data["username"]):
+                    self.send({
+                        "type": "auth",
+                        "status": False,
+                        "message": "Username allready exists"
+                    })
+                    return
 
-        elif data["type"] == "getFilesHash":
-            self.sendHashes()
+                r = db.addUser(data["username"], sha(data["password"]))
+                if not r:
+                    self.send({
+                        "type": "auth",
+                        "status": False,
+                        "message": "Unknown error"
+                    })
+                    return
 
-        elif data["type"] == "getFile":
-            pass
-        elif data["type"] == "checkTime":
-            offset = abs(time.time() - data["time"])  # offset in seconds
-            if offset > 5:
-                self.send({
-                    "type": "checkTime",
-                    "data": "error",
-                    "offset": offset
-                })
-                return
-            self.send({"type": "checkTime", "data": "ok"})
-        else:
-            self.send({"type": "error", "data": "Unknown type"})
+                # auth success
+                self.authenticated = True
+                self.authenticatedAs = data["username"]
+                if self.authenticatedAs not in users:
+                    users[self.authenticatedAs] = []
+                users[self.authenticatedAs].append(self)
+
+                self.send({"type": "auth", "status": True, "dat": "login"})
+
+            elif data["type"] == "change":
+                r = db.setFile(self.authenticatedAs,
+                               data["path"], data["data"], time.time())
+                if not r:
+                    self.send({"type": "error", "data": "Unknown error"})
+                else:
+                    sendUsers(self.authenticatedAs, {
+                        "type": "getFile",
+                        "path": data["path"],
+                        "data": data["data"]
+                    }, self)
+
+                if "id" not in data:
+                    data["id"] = "old_client"
+                self.send({"type": "accnowledge",
+                           "path": data["path"], "id": data["id"]})
+            elif data["type"] == "delete":
+                r = db.removeFile(self.authenticatedAs, data["path"])
+                sendUsers(self.authenticatedAs, {
+                    "type": "delete",
+                    "path": data["path"]
+                }, self)
+                if not r:
+                    self.send(
+                        {"type": "error", "data": "could not delete file"})
+                if "id" not in data:
+                    data["id"] = "old_client"
+                self.send({"type": "accnowledge",
+                          "path": data["path"], "id": data["id"]})
+
+            elif data["type"] == "getFilesHash":
+                self.sendHashes()
+
+            elif data["type"] == "getFile":
+                f = db.getFile(self.authenticatedAs, data["data"])
+                if f == None:
+                    self.send({"type": "error", "data": "File not found"})
+                    return
+                if "id" not in data:
+                    data["id"] = "old_client"
+                self.send(
+                    {"type": "getFile", "path": data["data"], "data": f, "id": data["id"]})
+            elif data["type"] == "checkTime":
+                offset = abs(time.time() - data["time"])  # offset in seconds
+                if offset > 5:
+                    self.send({
+                        "type": "checkTime",
+                        "data": "error",
+                        "offset": offset
+                    })
+                    return
+                self.send({"type": "checkTime", "data": "ok"})
+            else:
+                self.send({"type": "error", "data": "Unknown type"})
+        except Exception as e:
+            traceback.print_exc()
 
     def sendHashes(self):
-            pass
+        h = db.getHashes(self.authenticatedAs)
+        hashes = {}
+        for x in h:
+            hashes[x["path"]] = {"hash": x["md5"], "time": x["changeTime"]}
+        self.send({"type": "getFilesHash", "data": hashes})
 
     def send(self, data):
         self.send_message(json.dumps(data))
